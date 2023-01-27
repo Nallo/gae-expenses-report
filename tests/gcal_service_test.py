ecf1c6daef7b3aae531c74f788304f5e362267bb
Stefano Martinallo <stefano.martinallo@qonto.com>
2023-01-27 14:51:22 +0100

    Add base_url to GCalService constructor
    Signed-off-by: Stefano Martinallo <stefano.martinallo@qonto.com>

--- tests/gcal_service_test.py
+++ tests/gcal_service_test.py
@@ -1,11 +1,13 @@
 from tests.utils.http_client_spy import HTTPClientSpy
 
 class GCalService:
-    pass
+    def __init__(self, base_url) -> None:
+        pass
 
 class Test_GCalService:
     def test_init_does_not_request_data_from_url(self) -> None:
+        any_url = "http://any-url.com"
         client = HTTPClientSpy()
-        _ = GCalService()
+        _ = GCalService(base_url=any_url)
 
         assert client.requested_urls == []
