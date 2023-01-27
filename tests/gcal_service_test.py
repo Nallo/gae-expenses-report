136b79d3fe6374a12a315ce44faaf8b8c4ad5274
Stefano Martinallo <stefano.martinallo@qonto.com>
2023-01-27 15:10:04 +0100

    Ensure get_events() is using the base url injected via GCalService constructor
    Signed-off-by: Stefano Martinallo <stefano.martinallo@qonto.com>

--- tests/gcal_service_test.py
+++ tests/gcal_service_test.py
@@ -1,13 +1,26 @@
 from tests.utils.http_client_spy import HTTPClientSpy
 
 class GCalService:
-    def __init__(self, base_url) -> None:
-        pass
+    def __init__(self, base_url: str, client: HTTPClientSpy) -> None:
+        self._base_url = base_url
+        self._client = client
+
+    def get_events(self) -> None:
+        self._client.get(url=self._base_url)
 
 class Test_GCalService:
     def test_init_does_not_request_data_from_url(self) -> None:
         any_url = "http://any-url.com"
         client = HTTPClientSpy()
-        _ = GCalService(base_url=any_url)
+        _ = GCalService(base_url=any_url, client=client)
 
         assert client.requested_urls == []
+
+    def test_get_events_requests_data_from_url(self) -> None:
+        a_url = "http://any-url.com"
+        client = HTTPClientSpy()
+        sut = GCalService(base_url=a_url, client=client)
+
+        sut.get_events()
+
+        assert client.requested_urls == [a_url]
--- tests/utils/http_client_spy.py
+++ tests/utils/http_client_spy.py
@@ -1,3 +1,6 @@
 class HTTPClientSpy:
     def __init__(self) -> None:
         self.requested_urls = []
+
+    def get(self, url) -> None:
+        self.requested_urls.append(url)
