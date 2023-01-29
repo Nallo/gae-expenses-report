189ca3d174cc2b43af68cccba4b1c3be7255f6bf
Stefano Martinallo <stefano.martinallo@qonto.com>
2023-01-29 09:03:14 +0100

    Extract SUT creation into factory method
    Signed-off-by: Stefano Martinallo <stefano.martinallo@qonto.com>

--- tests/gcal_service_test.py
+++ tests/gcal_service_test.py
@@ -17,18 +17,23 @@ class GCalService:
 
 class Test_GCalService:
     def test_init_does_not_request_data_from_url(self) -> None:
-        any_url = "http://any-url.com"
-        client = HTTPClientSpy()
-        _ = GCalService(base_url=any_url, client=client)
+        client, _ = self._make_sut()
 
         assert client.requested_urls == []
 
     def test_get_events_requests_data_from_url(self) -> None:
-        a_url = "http://any-url.com"
+        a_url = "http://my-url.com"
         a_calendar_id = "my-calendar-id"
-        client = HTTPClientSpy()
-        sut = GCalService(base_url=a_url, client=client)
+        client, sut = self._make_sut(base_url=a_url)
 
         sut.get_events(calendar_id=a_calendar_id)
 
         assert client.requested_urls == [a_url + "/calendars/" + a_calendar_id + "/events"]
+
+    # Helpers
+
+    def _make_sut(self, base_url: str = "http://any-url.com") -> tuple[HTTPClientSpy, GCalService]:
+        client = HTTPClientSpy()
+        sut = GCalService(base_url=base_url, client=client)
+
+        return (client, sut)
